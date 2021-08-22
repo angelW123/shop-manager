bc3973ea34ffac512ec98265a46c1d7a31dc3184
王安琪 <5712325+wang_angel@user.noreply.gitee.com>
2021-08-22 22:26:45 +0800

    设置路由

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Switch,Route,Redirect } from "react-router-dom";
+import { adminRoutes } from './routes'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>我是App的组件</h1>
+      <Switch>
+        {
+          adminRoutes.map(route => {
+            return <Route key={route.path} path={route.path} exact={route.exact} render={routeProps => {
+              return <route.component {...routeProps} />
+            }}/>
+          })
+        }
+        <Redirect to="/404"/>
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/admin/products/Edit.js
@@ -0,0 +1,12 @@
+ import React from 'react'
+ 
+ function Edit() {
+   return (
+     <div>
+       <h1>修改</h1>
+     </div>
+   )
+ }
+ 
+ export default Edit
+ 
